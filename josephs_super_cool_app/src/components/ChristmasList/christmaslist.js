1ec0f676aab13d61951635aa3da6c26dc28c7838
Caroline Brownlee <brownleecaroline@gmail.com>
2020-12-04 10:46:07 -0600

    small change

--- josephs_super_cool_app/src/components/ChristmasList/christmaslist.js
+++ josephs_super_cool_app/src/components/ChristmasList/christmaslist.js
@@ -5,47 +5,56 @@ export const ChristmasList = () => {
         <>
             <div className="letter_to_santa">
                 <h2>Please Santa, </h2>
-                <p>Will you bring me Transformers. I will be a good boy at school. I will listen to my teachers. I will sit up in my chair. I will clean up. I will not throw toys. I will not run. I will be a good boy at home. I'll listen to Mommy, Daddy, and my sisters. I will clean up my things. I will say my prayers.</p>
+                <h3>Will you bring me Transformers? I will be a good boy at school. I will listen to my teachers. I will sit up in my chair. I will clean up. I will not throw toys. I will not run. I will be a good boy at home. I'll listen to Mommy, Daddy, and my sisters. I will clean up my things. I will say my prayers.</h3>
 
                 <h2>Love, Joseph</h2>
             </div>
 
             <ul className="listStuff">
                 <li className="list_item">
-                    <h1>Transformer Playset (In Production, Santa's Workshop)</h1>
+                    <h1>Transformer Playset</h1>
                     <a href="https://www.amazon.com/gp/product/B07VW37QBT/ref=crt_ewc_img_dp_3?ie=UTF8&psc=1&smid=ATVPDKIKX0DER">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Hoist the Tow Bot (In Production, Santa's Workshop)</h1>
+                    <h1>Hoist the Tow Bot</h1>
                     <a href="https://www.amazon.com/gp/product/handle-buy-box/ref=dp_start-bbf_1_glance">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Optimus Prime (In Production, Santa's Workshop)</h1>
+                    <h1>Optimus Prime</h1>
                     <a href="https://www.amazon.com/Playskool-Heroes-Transformers-Optimus-Exclusive/dp/B00P2SNBV0/ref=sr_1_18?dchild=1&keywords=transformer+playset+rescue+bots&qid=1607045381&sr=8-18">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Academy Whirl (In Production, Santa's Workshop)</h1>
+                    <h1>Academy Whirl</h1>
                     <a href="https://www.amazon.com/Playskool-Transformers-Rescue-Academy-Whirl/dp/B07FDK76TC/ref=sr_1_39?dchild=1&keywords=transformer+playset+rescue+bots&qid=1607045381&sr=8-39">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Castle (In Production, Santa's Workshop)</h1>
+                    <h1>Castle</h1>
                     <a href="https://www.amazon.com/Melissa-Doug-Medieval-Drawbridge-Construction/dp/B000NV9FR0/ref=sr_1_12?crid=2E1586K4H5B2B&dchild=1&keywords=dinosaur+playset+melissa+and+doug&qid=1607046568&sprefix=dinosaur+playset+me%2Caps%2C187&sr=8-12">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Pirate (Reserve for Birthday Gift in March)</h1>
+                    <h1>Pirate      </h1>
                     <a href="https://www.amazon.com/KidKraft-Pirates-Cove-Play-Set/dp/B00N7GZMMK/ref=sr_1_34?dchild=1&keywords=figurines+for+castle&qid=1607047809&s=toys-and-games&sr=1-34">Amazon Link</a>
+                    <h3>Status: Not Yet in Production</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Bumblebee Rescue Guard (In Production, Santa's Workshop)</h1>
+                    <h1>Bumblebee Rescue Guard</h1>
                     <a href="https://www.amazon.com/dp/B07CN6ZTR6/ref=sspa_dk_detail_7?psc=1&pd_rd_i=B07CN6ZTR6&pd_rd_w=UI85S&pf_rd_p=7d37a48b-2b1a-4373-8c1a-bdcc5da66be9&pd_rd_wg=0Rw4A&pf_rd_r=0ZMB64C8H02TEG4PP735&pd_rd_r=a80ef015-326e-4849-8547-38a438454700&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyQUwzREo1UDk0S1I4JmVuY3J5cHRlZElkPUEwMDMxNTM3MkkxSkNRQVVINjQ1TiZlbmNyeXB0ZWRBZElkPUEwODQ0ODgzWjNGSEgwOVJEWEtGJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Creative Drill Set (In Production, Santa's Workshop)</h1>
+                    <h1>Creative Drill Set</h1>
                     <a href="https://www.amazon.com/gp/product/B07DN6WVSN/ref=crt_ewc_title_dp_2?ie=UTF8&psc=1&smid=A1N9FOQX529BWW">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
                 <li className="list_item">
-                    <h1>Lego Table (In Production, Santa's Workshop)</h1>
+                    <h1>Lego Table</h1>
                     <a href="https://www.amazon.com/gp/product/B01MQGKX41/ref=crt_ewc_title_dp_1?ie=UTF8&smid=A83QP6JJC0C3W&th=1">Amazon Link</a>
+                    <h3>Status: In Production, Santa's Workshop</h3>
                 </li>
             </ul>
         </>
