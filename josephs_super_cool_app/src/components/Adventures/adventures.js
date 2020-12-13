ca8ed3ed221316ccb4ee625a6f5713aa7f4fa483
Caroline Brownlee <brownleecaroline@gmail.com>
2020-12-13 13:34:10 -0600

    added content and style to journal entry

--- josephs_super_cool_app/src/components/Adventures/adventures.js
+++ josephs_super_cool_app/src/components/Adventures/adventures.js
@@ -1,14 +1,26 @@
 import "./adventures.css"
-import { josephLake } from "../Images/josephLake.jpg"
+import josephLake from "../Images/josephLake.jpg"
+import josephPumpkin from "../Images/josephPumpkin.jpg"
+import josephMommyLake from "../Images/josephMommyLake.jpg"
+import josephLakeTwo from "../Images/josephLake2.jpg"
 
 
 export const Adventures = () => {
 
     return (
-        <>  
-            <h1 className="title">Exploring Sunday Morning</h1>
-            <p>This morning Mommy took me to the arboretum. I threw rocks in the lake and went to the bridge. We walked through a cave, visited the bridge, and saw sharks in Old Hickory Lake. Mommy gave me her tiny rosary. She held her big one. We prayed the Hail, Mary and the Our Father. When we left, we stopped at Starbucks, and I got a chocolate chip cookie dough cake pop.</p>
-            <img className="josephLakePhoto" src={josephLake} alt={"Joseph at the lake."} />
+        <>
+            <h1 className="pageTitle">Journal of Adventures</h1>
+            <div className="journalEntryContainer">
+                <p className="title">Exploring Sunday Morning</p>
+                <p className="date">December 12, 2020</p>
+                <div className="photoContainer">
+                    <img className="josephLakePhoto" src={josephLake} alt={"Joseph at the lake."} />
+                    <img className="josephPumpkinPhoto" src={josephPumpkin} alt="Joseph with a pumpkin." />
+                    <img className="josephMommyLake" src={josephMommyLake} alt={"Joseph and Mommy at the lake."} />
+                    <img className="josephLakeTwo" src={josephLakeTwo} alt="Joseph at the lake." />
+                </div>
+                <p className="entry">This morning Mommy took me to the arboretum. I threw rocks in the lake and walked to the bridge. We walked through a cave and saw sharks in Old Hickory Lake. Mommy gave me her tiny rosary. She held her big one. We prayed the Hail, Mary and the Our Father. When we left, we stopped at Starbucks where I got a chocolate chip cookie dough cake pop.</p>
+            </div>
         </>
     )
 }
